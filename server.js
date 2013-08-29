a84d990311380873fd90090cdebc9ebef88da3a6
MarcusQuigley <marcusquigley@gmail.com>
2013-08-29 19:00:31 +0000

    adding array

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ var app = require('express')()
 server.listen(port);
 
 console.log("listening on port:", port);
-
+var count = 50;
+var timeout = 100;
 var fs = require('fs');
+var arr = [count];
 //var port = process.env.PORT || 5000;
 
 app.get("/", function(request, response){
@@ -27,6 +29,9 @@ app.get("/", function(request, response){
 
 io.sockets.on('connection', function(socket){
 	console.log("set up socket", socket);
+	arr.map(function (item){
+		console.log(item);
+	});
 });
 
 //app.listen(port);
