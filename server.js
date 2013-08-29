36785e9eccdc0d9941e780003ebd8a7a2b3c15b9
MarcusQuigley <marcusquigley@gmail.com>
2013-08-29 18:05:02 +0000

    adding sockets

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 
 var express = require('express');
+var server = require('http');
+var io = require('sockets.io');
 var app = express();
+server.createServer(app);
+io.listen(server);
+server.listen(port);
+
 var fs = require('fs');
 var port = process.env.PORT || 5000;
 
@@ -10,6 +16,6 @@ app.get("/", function(request, response){
 	response.send(content);
 });
 
-app.listen(port);
+//app.listen(port);
 console.log("listening on port:", port);
 
