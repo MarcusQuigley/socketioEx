26dfcbe2428fea7ddd47c57ff3982810015a8097
MarcusQuigley <marcusquigley@gmail.com>
2013-08-29 17:47:37 +0000

    adding html and displaying it

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
 var port = process.env.PORT || 5000;
 
 app.get("/", function(request, response){
+	var content = fs.readFileSync('template.html');
 	response.setHeader("Content-Type", "text/html");
-	response.send("Hello from sockets example");
+	response.send(content);
 });
 
 app.listen(port);
