87becd3ecae876d8dbb9f4cd8d4d373eca4f9445
MarcusQuigley <marcusquigley@gmail.com>
2013-08-30 13:15:26 +0000

    adding twitter credentials

---
+++ credentials.js
@@ -0,0 +1,9 @@
+
+var credentials = {
+    consumer_key: 'w1gR6hyMJ34gBfAoG1TkqQ',
+    consumer_secret: '2ryuPbBGtVvdilEyAShuwSuaMpcsGNrhineZCL2Vs',
+    access_token_key: '1704434052-iusjfDhw2uqJXuUp5N6AyBbCOa4C0avRSGHk6LN',
+    access_token_secret: 'PtBUvtkyQX89QmBJrHxXLuNznlQA1c6witY0xZMw4'
+};
+
+module.exports = credentials;
