ef32a95ab0e640f7f615da5eee6ca95f2ec8d2b2
MarcusQuigley <marcusquigley@gmail.com>
2013-08-30 12:09:09 +0000

    testing

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 
-//var express = require('express');
-//var server = require('http');
-//var io = require('socket.io');
-//var app = express();
 var port = process.env.PORT || 5000;
-//server.createServer(app);
-//io.listen(server);
-//server.listen(port);
+
 
 var app = require('express')()
   , server = require('http').createServer(app)
@@ -25,7 +19,6 @@ var count = 50;
 var timeout = 100;
 var fs = require('fs');
 var arr = [count];
-//var port = process.env.PORT || 5000;
 
 app.get("/", function(request, response){
 	var content = fs.readFileSync('template.html');
@@ -36,30 +29,17 @@ app.get("/", function(request, response){
 
 io.sockets.on('connection', function(socket){
 	console.log("set up socket", socket);
-	arr.map(function(cnt){
+	for(var i=0;i<50;i++){//  arr.map(function(cnt){
 		(function(s){
 		 	setTimeout(function(){
-				socket.emit('news', { 'value': cnt });
-				console.log(cnt);
+				socket.emit('news', { 'value': i });
+				console.log(i);
 			}, 100);
-		})(count);
-//		socket.emit('news', { 'value': count });
-	});		
+		})(i);
+
+	}		
 	
-
-	//socket.on('my other event', function (data) {
-	  //  console.log(data);
-//	});
-
-
-//	arr.map(function (item){
-//		socket.emit('tweet',item )
-//		console.log(item);
-//	});
 });
-
-//app.listen(port);
-//console.log("listening on port:", port);
 
 
 
